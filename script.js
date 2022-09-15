49ab645ea37f75cc955361ed37da355f07bc201c
Sérgio Moura <smouracalmon@gmail.com>
2022-09-15 18:46:51 -0300

    Melhorado o código da calculadora

--- script.js
+++ script.js
@@ -5,38 +5,27 @@
  *  - document.querySelectorAll (retorna um array de elementos)
  */
 
-const bt1 = document.getElementById('bt_1');
-const bt2 = document.getElementById('bt_2');
-const bt3 = document.getElementById('bt_3');
-const bt4 = document.getElementById('bt_4');
-const bt5 = document.getElementById('bt_5');
-const bt6 = document.getElementById('bt_6');
-const bt7 = document.getElementById('bt_7');
-const bt8 = document.getElementById('bt_8');
-const bt9 = document.getElementById('bt_9');
-const bt0 = document.getElementById('bt_0');
-const btAdicao = document.getElementById('bt_+');
-const btSubtracao = document.getElementById('bt_-');
-const btMultiplicacao = document.getElementById('bt_*');
-const btDivisao  = document.getElementById('bt_/');
-const btIgual = document.getElementById('igual');
+// Captura dos botões
+const botoes = document.querySelectorAll('button');
 
-bt1.addEventListener('click', ()=>{escreveNoDisplay('1')});
-bt2.addEventListener('click', ()=>{escreveNoDisplay('2')});
-bt3.addEventListener('click', ()=>{escreveNoDisplay('3')});
-bt4.addEventListener('click', ()=>{escreveNoDisplay('4')});
-bt5.addEventListener('click', ()=>{escreveNoDisplay('5')});
-bt6.addEventListener('click', ()=>{escreveNoDisplay('6')});
-bt7.addEventListener('click', ()=>{escreveNoDisplay('7')});
-bt8.addEventListener('click', ()=>{escreveNoDisplay('8')});
-bt9.addEventListener('click', ()=>{escreveNoDisplay('9')});
-bt0.addEventListener('click', ()=>{escreveNoDisplay('0')});
-btAdicao.addEventListener('click', ()=>{escreveNoDisplay('+')});
-btSubtracao.addEventListener('click', ()=>{escreveNoDisplay('-')});
-btMultiplicacao.addEventListener('click', ()=>{escreveNoDisplay('*')});
-btDivisao.addEventListener('click', ()=>{escreveNoDisplay('/')});
+// Associar os eventos click de cada botão à execução da função onButtonClick
+for(let bt of botoes){
+    bt.addEventListener('click', onButtonClick);
+}
 
-btIgual.addEventListener('click', ()=>{calcular()});
+function onButtonClick(evt){
+
+    // Capturar o caractere do botão clicado
+    let caractere = evt.target.innerHTML;
+
+    // Se o botão clicado tiver '=' dentro, chama a função calcular
+    // Caso contrário, escrever no display o caractere do botão.
+    if(caractere == '='){
+        calcular();
+    } else {
+        escreveNoDisplay(caractere);
+    }
+}
 
 function escreveNoDisplay(caractere){
     
